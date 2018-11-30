2055d3a372c179d8ce068e16695037e3f7dce748
CloudRex <cloudrex@outlook.com>
2018-11-30 13:03:14 -0500

    Add Utils class

---
+++ Core/Utils.cs
@@ -0,0 +1,32 @@
+﻿namespace ForgeSharp.Core
+{
+    public static class Utils
+    {
+        public static string Mention(string userId, bool verifiedEmail = true)
+        {
+            if (userId == null)
+            {
+                return null;
+            }
+
+            string result = "<@";
+
+            if (!verifiedEmail)
+            {
+                result += "!";
+            }
+
+            return $"{result}{userId}>";
+        }
+
+        public static string MentionChannel(string channelId)
+        {
+            return $"<#{channelId}>";
+        }
+
+        public static string MentionRole(string roleId)
+        {
+            return $"<@&{roleId}>";
+        }
+    }
+}
